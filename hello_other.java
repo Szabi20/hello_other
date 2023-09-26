85f89f25c4ba8975cc8af584ca7d7eec3b277f4b
szt2GémesSzabolcs <115459925+Szabi20@users.noreply.github.com>
2023-09-26 12:43:47 +0200

    Add files via upload

---
+++ hello_other.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class hello_other {
+    public static void main(String[] args){
+        String nev="Szabi";
+        String nev2="Péter";
+        String nev3="Máté";
+
+        System.out.println("Hello,"+nev+"!");
+        System.out.println("Hello,"+nev2+"!");
+        System.out.println("Hello,"+nev3+"!");
+    }}
